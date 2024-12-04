2a3e53c8860da92961f3f8ebd56815885fd7aa92
pkozy <pkozyarchuk50@stuy.edu>
2024-12-03 23:18:28 -0500

    merge stuff perhaps

--- main.c
+++ main.c
@@ -34,7 +34,6 @@ char ** prompt(){
   getcwd(buffer, 256);
   printf("%s$ ", buffer);
   fflush(stdout);
-  //i think fflush is supposed to go here maybe idk.........
   char * line_buff = (char*)calloc(256, sizeof(char));
   fgets(line_buff, 255, stdin);
   parse_args(line_buff, args);
@@ -52,13 +51,8 @@ run c2 and redirect the temp file to stdin!!!
 remove the temp file when done!!!
 
 */
-<<<<<<< HEAD
-void piper(char* command1, char* command2){
-=======
-
 // Redirect stdout from one program to stdin of the next.
 void pipe(char* command1, char* command2){
->>>>>>> raechel
     char* args1[32];
     char* args2[32];
     // copies command into args arrays, separates arguments
