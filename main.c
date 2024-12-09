70a10ab790fb5f0598d87db20ddb3b8d83bf9f8a
Raechel Grace Calungsod <raegrasc13@gmail.com>
2024-12-08 21:19:55 -0500

    forgot cd

--- main.c
+++ main.c
@@ -142,3 +142,14 @@ void piping(char** args){
 
     remove("temp.txt"); // looked this up
 }
+
+int cd(char ** args){
+  if ((strcmp(args[0], "cd")) || (args[1] == NULL) || (args[2]!=NULL)){
+    printf("this shouldn't happen. usage: cd <directory>.\n");
+    return -1;
+  } else{
+    char * path = strsep(&args[1], "\n");
+    if (chdir(path)==-1)err();
+  }
+  return 0;
+}
